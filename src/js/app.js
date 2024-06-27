4412fcadf0370bb6fe514ffc70e36a3dc2b09a86
Benjamin Galan <begalan@outlook.es>
2024-06-27 00:02:09 -0600

    Funciones dinamicas a la galeria

---
+++ src/js/app.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    crearGaleria();
+})
+
+function crearGaleria() {
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes');
+    for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'imagen galeria'
+        galeria.appendChild(imagen)
+
+        imagen.onclick = function () {
+            mostrarImagen(i)
+        }
+    }
+}
+
+function mostrarImagen(i) {
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'imagen galeria'
+
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    //boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal)
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden');
+    }, 450);
+
+
+}
