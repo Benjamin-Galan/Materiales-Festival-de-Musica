fcbe4484c97015e0c9507916efb666b4b76a6114
Benjamin Galan <begalan@outlook.es>
2024-06-27 15:19:16 -0600

    Cambios desde los archivos de la carpeta src

--- build/js/app.js
+++ build/js/app.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+    resaltarEnlace();
     navegacionFija();
-    crearGaleria();
+    crearGaleria();  
+    scrollNav(); 
 })
 
-function navegacionFija(){
+function navegacionFija() {
     const header = document.querySelector('.header')
     const sobreFestival = document.querySelector('.sobre-festival')
 
-    document.addEventListener('scroll', function(){
-        if(sobreFestival.getBoundingClientRect().bottom < 1){
+    document.addEventListener('scroll', function () {
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
             header.classList.add('fixed')
-        } else{
+        } else {
             header.classList.remove('fixed')
         }
     })
@@ -66,4 +68,43 @@ function cerrarModal() {
     }, 450);
 
 
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function () {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+        let actual ='';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id
+            }
+        });
+
+        navLinks.forEach(link =>{
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    navLinks.forEach(link =>{
+        link.addEventListener('click', e =>{
+            e.preventDefault()
+
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
 }
